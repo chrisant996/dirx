chrisant996/dirx

commit cb16e60f3302ad77f7450e22deaf15c8e82f1906

Add prototype --nix flag.
This applies default options similar to `ls`.

main.cpp
@@ -30,7 +30,7 @@
 
 #include <memory>
 
-static const WCHAR c_opts[] = L"/:+?V,+1+2+4+a.b+c+C+E.f:F+h+i+j+J+k+l+n+o.p+q+Q.r+s+S.t+T.u+v+w+W:x+X.Y+z+Z+";
+static const WCHAR c_opts[] = L"/:+?V,+1+2+4+a.b+c+C+E.f:F+G+h+i+j+J+k+l+n+o.p+q+Q.r+R+s+S.t+T.u+v+w+W:x+X.Y+z+Z+";
 static const WCHAR c_DIRXCMD[] = L"DIRXCMD";
 
 static const WCHAR* get_env_prio(const WCHAR* a, const WCHAR* b=nullptr, const WCHAR* c=nullptr)
@@ -89,7 +89,7 @@ int __cdecl _tmain(int argc, const WCHAR** argv)
 
     const WCHAR* more_colors = nullptr;
     bool show_all_attributes = false;
-    int hide_dot_files = 0;         // By default, behave like CMD DIR.
+    int nix_defaults = 0;           // By default, behave like CMD DIR.
 #ifdef DEBUG
     int print_all_icons = 0;
 #endif
@@ -109,13 +109,18 @@ int __cdecl _tmain(int argc, const WCHAR** argv)
         LOI_ESCAPE_CODES,
         LOI_NO_FAT,
         LOI_NO_FULL_PATHS,
+        LOI_HIDE_DOT_FILES,
+        LOI_NO_HIDE_DOT_FILES,
         LOI_HORIZONTAL,
         LOI_HYPERLINKS,
         LOI_NO_HYPERLINKS,
         LOI_ICONS,
         LOI_NO_ICONS,
         LOI_JUSTIFY,
+        LOI_LOWER,
         LOI_NO_LOWER,
+        LOI_NIX,
+        LOI_NO_NIX,
         LOI_NO_NORMAL,
         LOI_MORE_COLORS,
         LOI_NERD_FONTS_VER,
@@ -151,19 +156,22 @@ int __cdecl _tmain(int argc, const WCHAR** argv)
         { L"no-fat",                nullptr,            LOI_NO_FAT },
         { L"full-paths",            nullptr,            'F' },
         { L"no-full-paths",         nullptr,            LOI_NO_FULL_PATHS },
+        { L"grid",                  nullptr,            'G' },
         { L"help",                  nullptr,            '?' },
-        { L"hide-dot-files",        &hide_dot_files,    1 },
-        { L"no-hide-dot-files",     &hide_dot_files,    -1 },
+        { L"hide-dot-files",        nullptr,            LOI_HIDE_DOT_FILES },
+        { L"no-hide-dot-files",     nullptr,            LOI_NO_HIDE_DOT_FILES },
         { L"horizontal",            nullptr,            LOI_HORIZONTAL },
         { L"hyperlinks",            nullptr,            LOI_HYPERLINKS },
         { L"no-hyperlinks",         nullptr,            LOI_NO_HYPERLINKS },
         { L"icons",                 nullptr,            LOI_ICONS,              LOHA_OPTIONAL },
         { L"no-icons",              nullptr,            LOI_NO_ICONS },
         { L"justify",               nullptr,            LOI_JUSTIFY,            LOHA_OPTIONAL },
-        { L"lower",                 nullptr,            'l' },
+        { L"lower",                 nullptr,            LOI_LOWER },
         { L"no-lower",              nullptr,            LOI_NO_LOWER },
         { L"more-colors",           nullptr,            LOI_MORE_COLORS,        LOHA_REQUIRED },
         { L"nerd-fonts",            nullptr,            LOI_NERD_FONTS_VER,     LOHA_REQUIRED },
+        { L"nix",                   nullptr,            LOI_NIX },
+        { L"no-nix",                nullptr,            LOI_NO_NIX },
         { L"normal",                nullptr,            'n' },
         { L"no-normal",             nullptr,            LOI_NO_NORMAL },
         { L"owner",                 nullptr,            'q' },
@@ -299,8 +307,6 @@ int __cdecl _tmain(int argc, const WCHAR** argv)
     const LongOption<WCHAR>* long_opt;
     WCHAR ch;
 
-    HideDotFiles(hide_dot_files);
-
     for (unsigned ii = 0; opts.GetValue(ii, ch, opt_value, &long_opt); ii++)
     {
         FormatFlags flagsON = FMT_NONE;
@@ -312,15 +318,15 @@ int __cdecl _tmain(int argc, const WCHAR** argv)
         case 'b':       flagsON = FMT_BARE; break;
         case 'c':       flagsON = FMT_COLORS; break;
         case 'C':       flagsON = FMT_COMPRESSED; break;
-        case 'F':       flagsON = FMT_FULLNAME|FMT_FORCENONFAT|FMT_HIDEDOTS; break;
-        case 'h':       flagsON = FMT_HIDEDOTS; break;
+        case 'F':       flagsON = FMT_FULLNAME|FMT_FORCENONFAT|FMT_HIDEPSEUDODIRS; break;
+        case 'h':       flagsON = FMT_HIDEPSEUDODIRS; break;
         case 'i':       SetUseIcons((*opt_value == '-') ? L"never" : L"auto"); continue;
         case 'j':       flagsON = FMT_JUSTIFY_FAT; break;
         case 'J':       flagsON = FMT_JUSTIFY_NONFAT; break;
         case 'k':       SetColorScale((*opt_value == '-') ? L"none" : L"all"); continue;
-        case 'l':       flagsON = FMT_LOWERCASE; break;
         case 'q':       flagsON = FMT_SHOWOWNER; break;
         case 'r':       flagsON = FMT_ALTDATASTEAMS|FMT_FORCENONFAT; break;
+        case 'R':       flagsON = FMT_SUBDIRECTORIES; break;
         case ':':       flagsON = FMT_ALTDATASTEAMS|FMT_FORCENONFAT; break;
         case 's':       flagsON = FMT_SUBDIRECTORIES; break;
         case 't':       flagsON = FMT_ATTRIBUTES; break;
@@ -441,6 +447,17 @@ unrecognized_long_opt_value:
                 else
                     goto unrecognized_long_opt_value;
                 break;
+            case LOI_NIX:
+                nix_defaults = true;
+                HideDotFiles(true);
+                flags |= FMT_COLORS|FMT_FORCENONFAT|FMT_HIDEPSEUDODIRS|FMT_SORTVERTICAL|FMT_SKIPHIDDENDIRS|FMT_NOVOLUMEINFO|FMT_NOHEADER|FMT_NOSUMMARY|FMT_MINIHEADER;
+                flags &= ~(FMT_JUSTIFY_FAT|FMT_JUSTIFY_NONFAT|FMT_FAT|FMT_SHORTNAMES|FMT_ONLYSHORTNAMES|FMT_FULLNAME);
+                break;
+            case LOI_NO_NIX:
+                nix_defaults = false;
+                HideDotFiles(false);
+                flags &= ~(FMT_HIDEPSEUDODIRS|FMT_SORTVERTICAL|FMT_FORCENONFAT|FMT_FAT|FMT_SKIPHIDDENDIRS|FMT_NOVOLUMEINFO|FMT_NOHEADER|FMT_NOSUMMARY|FMT_MINIHEADER);
+                break;
             case LOI_TRUNCATE_CHAR:
                 SetTruncationCharacterInHex(opt_value);
                 break;
@@ -453,11 +470,12 @@ unrecognized_long_opt_value:
             case LOI_COMPACT_COLUMNS:       SetCanAutoFit(true); break;
             case LOI_NO_COMPACT_COLUMNS:    SetCanAutoFit(false); break;
             case LOI_NO_FAT:                flagsOFF = FMT_FAT; break;
-            case LOI_NO_FULL_PATHS:         flagsOFF = FMT_FULLNAME|FMT_FORCENONFAT|FMT_HIDEDOTS; break;
+            case LOI_NO_FULL_PATHS:         flagsOFF = FMT_FULLNAME|FMT_FORCENONFAT|FMT_HIDEPSEUDODIRS; break;
             case LOI_HORIZONTAL:            flagsOFF = FMT_SORTVERTICAL; break;
             case LOI_HYPERLINKS:            flagsON = FMT_HYPERLINKS; break;
             case LOI_NO_HYPERLINKS:         flagsOFF = FMT_HYPERLINKS; break;
             case LOI_NO_ICONS:              SetUseIcons(L"never"); break;
+            case LOI_LOWER:                 flagsON = FMT_LOWERCASE; break;
             case LOI_NO_LOWER:              flagsOFF = FMT_LOWERCASE; break;
             case LOI_NO_NORMAL:             flagsOFF = FMT_FORCENONFAT; break;
             case LOI_MORE_COLORS:           more_colors = opt_value; break;
@@ -638,6 +656,8 @@ unrecognized_long_opt_value:
     }
 
     unsigned cColumns = 1;
+    if (nix_defaults)
+        cColumns = 0;
 
     if (!(flags & (FMT_BARE|FMT_ATTRIBUTES|FMT_FULLNAME|FMT_FULLTIME|FMT_COMPRESSED|FMT_FORCENONFAT|FMT_SHOWOWNER|FMT_ONLYALTDATASTREAMS|FMT_ALTDATASTEAMS)))
     {
@@ -646,31 +666,36 @@ unrecognized_long_opt_value:
             switch (ch)
             {
             case '1':
-                cColumns = 1;
+            case 'l':
+                if (*opt_value != '-')
+                    cColumns = 1;
+                else if (cColumns == 1)
+                    cColumns = 0;
                 break;
             case '2':
                 if (!picture)
                 {
-                    if (*opt_value == '-' && cColumns == 2)
-                        cColumns = 1;
-                    else
+                    if (*opt_value != '-')
                         cColumns = 2;
+                    else if (cColumns == 2)
+                        cColumns = 1;
                 }
                 break;
             case '4':
                 if (!picture)
                 {
-                    if (*opt_value == '-' && cColumns == 4)
-                        cColumns = 1;
-                    else
+                    if (*opt_value != '-')
                         cColumns = 4;
+                    else if (cColumns == 4)
+                        cColumns = 1;
                 }
                 break;
+            case 'G':
             case 'w':
-                if (*opt_value == '-' && cColumns == 0)
-                    cColumns = 1;
-                else
+                if (*opt_value != '-')
                     cColumns = 0;
+                else if (cColumns == 0)
+                    cColumns = 1;
                 break;
             }
         }
@@ -678,7 +703,7 @@ unrecognized_long_opt_value:
 
     if (flags & FMT_FORCENONFAT)
         flags &= ~FMT_FAT;
-    if (cColumns != 1 && !(flags & (FMT_FAT|FMT_ATTRIBUTES|FMT_MINISIZE)))
+    if (cColumns != 1 && !nix_defaults && !(flags & (FMT_FAT|FMT_ATTRIBUTES|FMT_MINISIZE|FMT_CLASSIFY)))
         flags |= FMT_DIRBRACKETS;
     if (!(flags & FMT_FAT))
         flags |= FMT_FULLSIZE;
@@ -706,9 +731,6 @@ unrecognized_long_opt_value:
 
     if (cColumns != 1)
         flags &= ~FMT_FULLSIZE;
-
-    if (flags & FMT_CLASSIFY)
-        flags &= ~FMT_DIRBRACKETS;
 
     if ((flags & FMT_ATTRIBUTES) && show_all_attributes)
         flags |= FMT_ALLATTRIBUTES;
@@ -758,6 +780,11 @@ unrecognized_long_opt_value:
             return e.Report();
         }
     }
+
+    if (def.Settings().IsSet(FMT_MINIHEADER) &&
+        !def.Settings().IsSet(FMT_SUBDIRECTORIES) &&
+        (!patterns || !patterns->m_next))
+        def.Settings().m_flags &= ~FMT_MINIHEADER;
 
     const int rc = ScanDir(def, patterns, e);
 

flags.h
@@ -26,7 +26,7 @@ enum FormatFlags : ULONGLONG
     FMT_FULLNAME                = 0x000000001000,   // Show the fully qualified path in the file name column.
     FMT_HYPERLINKS              = 0x000000002000,   // Add hyperlink escape codes for file and directory names.
     FMT_LOWERCASE               = 0x000000004000,
-    FMT_HIDEDOTS                = 0x000000008000,   // Hide the "." and ".." directories.
+    FMT_HIDEPSEUDODIRS          = 0x000000008000,   // Hide the "." and ".." directories.
     FMT_DIRBRACKETS             = 0x000000010000,
     FMT_CLASSIFY                = 0x000000020000,   // Append type symbol after file name (\ for dir, @ for symlink file).
     FMT_SIZE                    = 0x000000040000,   // Include size even in multi-column formats.
@@ -52,6 +52,7 @@ enum FormatFlags : ULONGLONG
     FMT_NOVOLUMEINFO            = 0x004000000000,
     FMT_NOHEADER                = 0x008000000000,
     FMT_NOSUMMARY               = 0x010000000000,
+    FMT_MINIHEADER              = 0x020000000000,   // Show single line header per directory.
 };
 DEFINE_ENUM_FLAG_OPERATORS(FormatFlags);
 

formatter.cpp
@@ -2055,7 +2055,7 @@ bool DirEntryFormatter::OnVolumeBegin(const WCHAR* dir, Error& e)
     m_cbCompressedTotal = 0;
     m_line_break_before_volume = true;
 
-    if (Settings().IsSet(FMT_NOVOLUMEINFO|FMT_NOHEADER))
+    if (Settings().IsSet(FMT_NOVOLUMEINFO))
         return false;
 
     if (Settings().IsSet(FMT_USAGE))
@@ -2106,6 +2106,8 @@ bool DirEntryFormatter::OnVolumeBegin(const WCHAR* dir, Error& e)
     s.Printf(L" Volume Serial Number is %04X-%04X\n",
              HIWORD(dwSerialNumber), LOWORD(dwSerialNumber));
     OutputConsole(m_hout, s.Text(), s.Length());
+
+    m_line_break_before_miniheader = true;
     return true;
 }
 
@@ -2156,10 +2158,19 @@ void DirEntryFormatter::OnDirectoryBegin(const WCHAR* const dir)
     if (!m_dir.Length())
         m_dir.Set(dir);
 
-    if (!Settings().IsSet(FMT_BARE|FMT_NOHEADER))
+    if (!Settings().IsSet(FMT_BARE))
     {
         StrW s;
-        s.Printf(L"\n Directory of %s%s\n\n", dir, wcschr(dir, '\\') ? L"" : L"\\");
+        if (Settings().IsSet(FMT_MINIHEADER))
+        {
+            if (m_line_break_before_miniheader)
+                s.Append(L"\n");
+            s.Printf(L"%s%s:\n", dir, wcschr(dir, '\\') ? L"" : L"\\");
+        }
+        else if (!Settings().IsSet(FMT_NOHEADER))
+        {
+            s.Printf(L"\n Directory of %s%s\n\n", dir, wcschr(dir, '\\') ? L"" : L"\\");
+        }
         OutputConsole(m_hout, s.Text(), s.Length());
     }
 }
@@ -2536,6 +2547,8 @@ void DirEntryFormatter::OnDirectoryEnd(bool next_dir_is_different)
         m_cbAllocated = 0;
         m_cbCompressed = 0;
     }
+
+    m_line_break_before_miniheader = true;
 }
 
 void DirEntryFormatter::OnVolumeEnd(const WCHAR* dir)

formatter.h
@@ -99,6 +99,7 @@ private:
     StrW                m_sColor;
 
     bool                m_line_break_before_volume = false;
+    bool                m_line_break_before_miniheader = false;
 
     unsigned            m_cFiles = 0;
     unsigned            m_cDirs = 0;

output.cpp
@@ -584,6 +584,8 @@ void OutputConsole(HANDLE h, const WCHAR* p, unsigned len, const WCHAR* color)
 {
     if (len == unsigned(-1))
         len = unsigned(wcslen(p));
+    if (!len)
+        return;
 
     if (!s_paginate)
     {

scan.cpp
@@ -153,7 +153,7 @@ static bool ScanFiles(DirScanCallbacks& callbacks, const WCHAR* dir, const DirPa
                         continue;
                     if (callbacks.Settings().m_dwAttrMatch && (fd.dwFileAttributes & callbacks.Settings().m_dwAttrMatch) != callbacks.Settings().m_dwAttrMatch)
                         continue;
-                    if ((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && callbacks.Settings().IsSet(FMT_HIDEDOTS) && IsPseudoDirectory(fd.cFileName))
+                    if ((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && callbacks.Settings().IsSet(FMT_HIDEPSEUDODIRS) && IsPseudoDirectory(fd.cFileName))
                         continue;
                     if (IsHiddenName(fd.cFileName))
                         continue;

scan.h
@@ -32,6 +32,5 @@ public:
     virtual bool        IsRootSubDir() const = 0;
 };
 
-bool ScanFiles(DirScanCallbacks& callbacks, const WCHAR* dir, const DirPattern* pattern, bool top, Error& e);
 int ScanDir(DirScanCallbacks& callbacks, const DirPattern* patterns, Error& e);
 

usage.cpp
@@ -8,6 +8,8 @@
 
 const char c_usage[] = "%s -? for help.";
 
+// --nix
+
 const char c_long_usage[] =
 "Displays a list of file and subdirectories in a directory.\n"
 "\n"
@@ -33,8 +35,10 @@ const char c_long_usage[] =
 "  -a, --all		Display all files (include hidden and system files).\n"
 "  -b, --bare		Bare mode; only display names, no header/detail/etc.\n"
 "  -c, --color		Display with colors (use '-? colors' for more info).\n"
+//"  -G, --grid		Synonym for --wide.\n"
 "  -i, --icons[=WHEN]	Display file icons (use '-? icons' for more info).\n"
 "			  always, auto, never (default)\n"
+//"  -l, --long		Synonym for -1.\n"
 "  -n, --normal		Force normal list format even on FAT volumes.\n"
 "  -Q..., --quash=...	Quash types of output:\n"
 "			  v  Suppress the volume information\n"
@@ -43,6 +47,7 @@ const char c_long_usage[] =
 "			  -  Prefix to suppress next type (the default)\n"
 "			  +  Prefix to un-suppress next type\n"
 "  -p, --paginate	Pause after each screen full of information.\n"
+//"  -R			Synonym for --recurse.\n"
 "  -s, --recurse		Subdirectories; recursively display files in specified\n"
 "			directory and all subdirectories.\n"
 "  -u, --usage		Display directory size usage data.\n"
@@ -132,7 +137,6 @@ const char c_long_usage[] =
 "  --justify[=WHEN]	Justify file names, in which list formats.  If WHEN is\n"
 "			omitted, 'always' is assumed.\n"
 "			  always, fat, normal, never (default)\n"
-"  -l, --lower		Show file names using lower case.\n"
 "  -SS			Show long file sizes (implies -S).  Note that some list\n"
 "			formats limit the file size width.\n"
 "  -TT			Show long dates and times (implies -T).  Note that some\n"
@@ -147,6 +151,7 @@ const char c_long_usage[] =
 "  --escape-codes[=WHEN]\n"
 "			For colors and hyperlinks in modern terminals.\n"
 "			  always, auto (default), never\n"
+"  --lower		Show file names using lower case.\n"
 "  --more-colors=LIST	Add color rules in the same format as the DIRX_COLORS\n"
 "			environment variable (use '-? colors' for more info).\n"
 "  --nerd-fonts=VER	Select which Nerd Fonts version to use (see '-? colors'\n"

commit 00ec167783bc9f68eeb76d9958d0f2bc10be4e37

Fix #5; --hide-dot-files has no effect.
It got broken by a refactor in cb16e60f on 2/19.

main.cpp
@@ -763,6 +763,8 @@ unrecognized_long_opt_value:
             case LOI_NO_GIT_IGNORE:         flagsOFF = FMT_GITIGNORE; break;
             case LOI_GIT_REPOS:             flagsON = FMT_GIT|FMT_GITREPOS; break;
             case LOI_GIT_REPOS_NO_STATUS:   flagsOFF = FMT_GITREPOS; break;
+            case LOI_HIDE_DOT_FILES:        HideDotFiles(true); break;
+            case LOI_NO_HIDE_DOT_FILES:     HideDotFiles(false); break;
             case LOI_HORIZONTAL:            flagsOFF = FMT_SORTVERTICAL; break;
             case LOI_HYPERLINKS:            flagsON = FMT_HYPERLINKS; break;
             case LOI_NO_HYPERLINKS:         flagsOFF = FMT_HYPERLINKS; break;

version.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define VERSION_MAJOR           0
-#define VERSION_MINOR           14
+#define VERSION_MINOR           15
 
 #define IND_VER2( a, b ) #a ## "." ## #b
 #define IND_VER4( a, b, c, d ) L#a ## L"." ## L#b ## L"." ## L#c ## L"." ## L#d